e35b0cf134fe5dc81c780b8a26587aaf1be976be
ZiyuLiu258 <1209830516abc@gmail.com>
2024-10-03 00:59:11 +0800

    hw0b is half-solved

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -6,7 +6,7 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        return new int[]{1,2,3,4,5,6};
     }
 
     /** Returns the order depending on the customer.
@@ -15,14 +15,52 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        ArrayList<String> tgt=new ArrayList<>();
+        if(customer.equals("Ergun")){
+            tgt.add("beyti");
+            tgt.add("pizza");
+            tgt.add("hamburger");
+            tgt.add("tea");
+        } else if (customer.equals("Erik")) {
+            tgt.add("sushi");
+            tgt.add("pasta");
+            tgt.add("avocado");
+            tgt.add("coffee");
+        }
+        else{
+            return new String[3];
+        }
+        return tgt.toArray(new String[0]);
     }
+    /*
+    * public class EqualsExample {
+    public static void main(String[] args) {
+        String str1 = new String("hello");
+        String str2 = new String("hello");
+
+        // 使用 == 比较引用
+        System.out.println(str1 == str2);  // 输出 false，因为引用不同
+
+        // 使用 equals() 比较内容
+        System.out.println(str1.equals(str2));  // 输出 true，因为内容相同
+    }
+}*/
+
+
+
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        int max=array[0],min=array[0];
+        for(int i:array) {
+            if (i > max)
+                max = i;
+            if (i < min)
+                min = i;
+        }
+        return Math.abs(min-max);
     }
 
     /**
@@ -39,7 +77,15 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if(x%2==0){
+            return hailstoneHelper(x/2,list);
+        } else if (x!=1) {
+            return hailstoneHelper(x*3+1,list);
+        }
+        else{
+            return list;
+        }
     }
 
 }
--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
@@ -5,18 +6,28 @@ public class ListExercises {
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
         // TODO: Fill in this function.
-        return 0;
+        int sum=0;
+        for(int i:L){
+            sum+=i;
+        }
+        return sum;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
         // TODO: Fill in this function.
-        return null;
+        List<Integer> tg=new ArrayList<>();
+        for(int i:L){
+            if(i%2==0&&i>=0)
+                tg.add(i);
+        }
+        return tg;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
         // TODO: Fill in this function.
+
         return null;
     }
 
